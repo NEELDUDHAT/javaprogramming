27023529b0911c6c41c23951d23c08f0c36b03fb
Neel Dudhat <114645482+NEELDUDHAT@users.noreply.github.com>
2022-10-23 23:40:48 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,11 @@
+public class math {
+    public static void main(String[] args) {
+        int a = 6, b = 5;
+        System.out.println("the sum is " + (a + b));
+        System.out.println("the subtraction is " + (a - b));
+        System.out.println("the multification is " + (a * b));
+        System.out.println("the dividation is " + (a / b));
+        System.out.println("the modulus is " + (a % b));
+
+    }
+}
