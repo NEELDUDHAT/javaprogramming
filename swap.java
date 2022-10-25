e4cb270e3d8f65aa72d9c513ac870aaea1410712
Neel Dudhat <114645482+NEELDUDHAT@users.noreply.github.com>
2022-10-25 22:53:01 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,13 @@
+public class swap {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c;
+        c = a;
+        a = b;
+        b = c;
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+
+    }
+}
