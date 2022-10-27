f857cff31996ea22d29c8f1e850153dedced554c
Neel Dudhat <114645482+NEELDUDHAT@users.noreply.github.com>
2022-10-27 22:00:55 +0530

    Add files via upload

---
+++ operators/assignmentoperator.java
@@ -0,0 +1,26 @@
+public class assignmentoperator {
+    public static void main(String[] args) {
+        int p = 10, q;
+
+        // use of = operator
+        q = p;
+        System.out.println(q);
+
+        // // use of += operator
+        p += q; // p=p+q
+        System.out.println(p);
+
+        // use of -= operator
+        p -= q; // p=p-q
+        System.out.println(p);
+
+        // use of *= operator
+        p *= q; // p=p*q
+        System.out.println(p);
+
+        // use of /= operator
+        p /= q; // p=p/q
+        System.out.println(p);
+
+    }
+}
